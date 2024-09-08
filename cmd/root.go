0a6bdfc54c5187220c004709bdf26f671b2f55ed
makl11 <makl11@users.noreply.github.com>
2024-09-08 13:44:24 +0200

    add cobra + viper to create cli

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "musiman",
+	Short: "A tool to manage your music files",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.musiman.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("json")
+		viper.SetConfigName(".musiman")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"os"
-	"path/filepath"
-
-	"github.com/makl11/musiman/scanner"
-)
+import "github.com/makl11/musiman/cmd"
 
 func main() {
-	var err error
-	scanRoot, _ := filepath.Abs(".")
-	if len(os.Args) > 1 {
-		scanRoot, err = filepath.Abs(os.Args[1])
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	scanner.ScanDirForMusic(scanRoot)
+	cmd.Execute()
 }
